60d5bb3ddda9ba2e6524c8e9992de44e9bec5971
Cameron Hart <cameronhart@Cameron-Hart-Temp-MBA.local>
2026-04-12 14:06:31 -0600

    Add Firebase auth, Firestore, admin portal

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FootballCoach from "./football-coach.jsx";
+import AdminApp from "./admin.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"      element={<FootballCoach />} />
+        <Route path="/admin" element={<AdminApp />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
